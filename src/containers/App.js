af961c514cf7cd92060cf589bbb8df10e63730d3
Martin Atsinov <martin.atsinov@isobar.com>
2017-12-14 10:34:43 +0200

    add routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,35 @@
-import React, {
-  Component,
-  PropTypes
-} from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import {
-  sampleAction
-} from '../actions/'
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { sampleAction } from '../actions/';
 
-import PageWrapper from '../components/PageWrapper'
-import '../components/app.css'
+import PageWrapper from '../components/PageWrapper';
+import '../components/app.css';
 
 class App extends Component {
+  componentDidMount() {
+    const { actions } = this.props;
+
+    actions.sampleAction();
+  }
   render() {
-    return (
-      <PageWrapper />
-    )
+    return <PageWrapper />;
   }
 }
 
-App.defaultProps = {}
+App.defaultProps = {};
 App.propTypes = {
   actions: PropTypes.shape({})
-}
+};
 function mapStateToProps(state) {
-  const props = {}
-  return props
+  const props = {
+    ...state
+  };
+  return props;
 }
 function mapDispatchToProps(dispatch) {
-  const actions = { sampleAction }
-  const actionMap = { actions: bindActionCreators(actions, dispatch) }
-  return actionMap
+  const actions = { sampleAction };
+  const actionMap = { actions: bindActionCreators(actions, dispatch) };
+  return actionMap;
 }
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/containers/Router.js
+++ src/containers/Router.js
@@ -1,37 +1,29 @@
-import React, {
-  Component,
-  PropTypes
-} from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import {
-  sampleAction
-} from '../actions/'
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { sampleAction } from '../actions/';
 
-import { withRouter } from 'react-router-dom'
-import RouteList from '../routes'
-import '../components/app.css'
+import { withRouter } from 'react-router-dom';
+import RouteList from '../routes';
+import '../components/app.css';
 
 class App extends Component {
   render() {
-    return (
-      <RouteList />
-    )
+    return <RouteList />;
   }
 }
 
-App.defaultProps = {
-}
+App.defaultProps = {};
 App.propTypes = {
   actions: PropTypes.shape({})
-}
+};
 function mapStateToProps(state) {
-  const props = {}
-  return props
+  const props = { ...state };
+  return props;
 }
 function mapDispatchToProps(dispatch) {
-  const actions = { sampleAction }
-  const actionMap = { actions: bindActionCreators(actions, dispatch) }
-  return actionMap
+  const actions = { sampleAction };
+  const actionMap = { actions: bindActionCreators(actions, dispatch) };
+  return actionMap;
 }
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
